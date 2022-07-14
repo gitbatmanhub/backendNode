3ed9ca1f1a117a331253e779f5416655e2116bca
ecro <ecro98.98@gmail.com>
2022-07-13 20:19:37 -0500

    Primer Commit - Node a profundidad

---
+++ index.js
@@ -0,0 +1,11 @@
+//Importar una función y guardarla en una variable
+const math= require('./math.js');
+/*
+console.log(math.suma(7, 2));
+console.log(math.resta(7, 2));
+console.log(math.mult(7, 2));
+console.log(math.div(7, 2));
+console.log(math.div(2, 0));
+
+ */
+console.log(math)
